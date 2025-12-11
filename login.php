18c0417ca9e5945c3caca5df72188f12308e74d8
oldman <oldm2563@email.com>
2025-12-11 10:09:08 -0300

    login html

--- login.php
+++ login.php
@@ -3,9 +3,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stye.css"> 
     <title>Document</title>
 </head>
 <body>
-    
+    <header>
+        <img src="banhead.png" alt="logo">
+    </header>
+    <div class="login_container">
+        <div class="login_box">
+            <form action="">
+                <h3>Login</h3>
+                <div class="input_box">
+                    <input type="text" id="username" name="username" placeholder=" " required>
+                     <label for="username">User Name</label>
+                </div>
+                <div class="input_box">
+                <input type="password" id="pass" name="pass" placeholder=" " required>
+                 <label for="pass">Password</label>
+                 </div>
+            </form>
+        </div>
+    </div>
 </body>
 </html>
