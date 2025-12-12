822cd759f23c5890cbb334467b3d3501f3d209d6
oldman <oldm2563@email.com>
2025-12-12 09:18:17 -0300

    connecting to the mysql database

---
+++ db.php
@@ -0,0 +1,22 @@
+<?php
+    $db_server = "localhost";
+    $db_user = "root";
+    $db_pass = "";
+    $db_name = "bankly_v2";
+    $connect = "";
+
+    $connect = mysqli_connect(
+    $db_server,
+     $db_user,
+      $db_pass,
+       $db_name,
+        3307
+    );
+
+       if (!$connect) {
+    die("Connection failed: " . mysqli_connect_error());
+    } else {
+    //yo worjd
+    }
+
+?>
