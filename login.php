d32c426ed2ed810e5d77f7d7763fc8f9b2cc8911
oldman <oldm2563@email.com>
2025-12-12 09:20:00 -0300

    making the the login screen

--- login.php
+++ login.php
@@ -1,6 +1,31 @@
 <?php
-    
+    include("db.php");
+    session_start(); 
+    $error = "";   
+    if(isset($_POST["submit"])){
+    $username = $_POST["username"];
+    $password = $_POST["pass"];
+    $data = "SELECT username, passwords FROM user WHERE username = '{$username}'";
+    $founddata = mysqli_query($connect, $data);
+
+        if(mysqli_num_rows($founddata)>0){
+        $row = mysqli_fetch_assoc($founddata);
+        if($password === $row["passwords"]){
+            $_SESSION["username"] = $row["username"];
+            header("Location: dashboard.php");
+            exit;
+        }
+        else{
+            $error = "wrong password";
+        }
+        }
+        else{
+            $error = "user doesn't exist";
+        }
+    }
 ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -16,7 +41,7 @@
         </div>
     <div class="login_container">
         <div class="login_box">
-            <form action="login.php" method="POST">
+            <form action="login.php" method="post">
                 <h3>Login</h3>
                 <div class="input_box">
                     <input type="text" id="username" name="username" placeholder=" " required>
@@ -29,6 +54,9 @@
                  <div>
                     <input type="submit" name="submit" id="submit" value="Login Now">
                  </div>
+                 <?php if(!empty($error)){
+                    echo "<p>{$error}</p>";
+                 } ?>
             </form>
         </div>
     </div>
